1799b72c24dc37f8713fb17cd0fd1fe9f7d4fd13
Scubadoo2 <scubadoo2@rocketmail.com>
2022-03-03 11:48:59 -0800

    SI3 Done

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -15,17 +15,17 @@ using namespace std;
 #define B '.'
 #define boardSize 3
 
-bool read(         char board[][boardSize], const char* fileName);
-bool write(  const char board[][boardSize], const char* fileName);
-void display(const char board[][boardSize]);
-bool didWin( const char board[][boardSize], char turn);
+bool read(         char board[][boardSize][boardSize], const char* fileName);
+bool write(  const char board[][boardSize][boardSize], const char* fileName);
+void display(const char board[][boardSize][boardSize]);
+bool didWin( const char board[][boardSize][boardSize], char turn);
 
 /**********************************************************************
  * Keeps the data and calles the read/display/write functions
  ***********************************************************************/
 int main()
 {
-   char board[boardSize][boardSize];
+   char board[boardSize][boardSize][boardSize];
 
    // read the board
    char fileName[256];
@@ -56,7 +56,7 @@ int main()
  * READ
  * Read the board from the specified filename
  *************************************************************/
-bool read(char board[][boardSize], const char* fileName)
+bool read(char board[][boardSize][boardSize], const char* fileName)
 {
    assert(*fileName);
 
@@ -66,16 +66,16 @@ bool read(char board[][boardSize], const char* fileName)
       return false;
 
    // read 9 symbols, hopefully they are . X O
-   for (int r = 0; r < boardSize; r++)
-      for (int c = 0; c < boardSize; c++)
-      {
-         fin >> board[r][c];
-         assert(!fin.fail());
-         assert(board[r][c] == X ||
-                board[r][c] == O ||
-                board[r][c] == B);
-      }
-
+   for (int z = 0; z < boardSize; z++)
+      for (int r = 0; r < boardSize; r++)
+         for (int c = 0; c < boardSize; c++)
+         {
+            fin >> board[z][r][c];
+            assert(!fin.fail());
+            assert(board[z][r][c] == X ||
+                  board[z][r][c] == O ||
+                  board[z][r][c] == B);
+         }
    // close the file
    fin.close();
    return true;
@@ -85,7 +85,7 @@ bool read(char board[][boardSize], const char* fileName)
  * WRITE
  * Write to fileName the board data
  *********************************************************/
-bool write(const char board[][boardSize], const char* fileName)
+bool write(const char board[][boardSize][boardSize], const char* fileName)
 {
    assert(fileName[0] != '\0');
 
@@ -95,9 +95,10 @@ bool write(const char board[][boardSize], const char* fileName)
       return false;
 
    // write my 9 symbols
-   for (int r = 0; r < boardSize; r++)
-      for (int c = 0; c < boardSize; c++)
-         fout << board[r][c] << (c == 2 ? '\n' : ' ');
+   for (int z = 0; z < boardSize; z++)
+      for (int r = 0; r < boardSize; r++)
+         for (int c = 0; c < boardSize; c++)
+            fout << board[z][r][c] << (c == 2 ? '\n' : ' ');
 
    // close it!
    fout.close();
@@ -109,27 +110,36 @@ bool write(const char board[][boardSize], const char* fileName)
  * DISPLAY
  * Display the contents the the screen
  *****************************************************/
-void display(const char board[][boardSize])
+void display(const char board[][boardSize][boardSize])
 {
-   // loop through each row
-   for (int r = 0; r < boardSize; r++)
-   {
-      // only the first row is not preceeded with the --+-- magic
-      if (r != 0)
-         cout << "---+---+---\n";
+   //Display each layer
+   for(int d=0; d < boardSize; d++){
+      cout << "Level " << d+1 << endl;
 
-      // now, on each row, do the column stuff
-      for (int c = 0; c < boardSize; c++)
+      // loop through each row
+      for (int r = 0; r < boardSize; r++)
       {
-         // display a space for the dot
-         if (board[r][c] == B)
-            cout << "   ";
-         else
-            cout << " " << board[r][c] << " ";
+         // only the first row is not preceeded with the --+-- magic
+         if (r != 0)
+            cout << "---+---+---\n";
 
-         // end with a | or a newline
-         cout << (c == 2 ? '\n' : '|');
+         // now, on each row, do the column stuff
+         for (int c = 0; c < boardSize; c++)
+         {
+            // display a space for the dot
+            if (board[d][r][c] == B)
+               cout << "   ";
+            else
+               cout << " " << board[d][r][c] << " ";
+
+            // end with a | or a newline
+            cout << (c == 2 ? '\n' : '|');
+         }
       }
+      
+      //A gap in between
+      cout << endl;
+
    }
 
    // display who won
@@ -146,43 +156,96 @@ void display(const char board[][boardSize])
  * Did a given player (determined by the "turn"
  * variable) win the game?
  *******************************************/
-bool didWin(const char board[][boardSize], char turn)
+bool didWin(const char board[][boardSize][boardSize], char turn)
 {
    
    bool isGood = true;
-   for (int r = 0; r < boardSize; r++)
-   {
+   // check layer by layer (treat as 2d tic-tac-toe games)
+   for (int z = 0; z < boardSize; z++){
+      // check columns for each layer
+      for (int r = 0; r < boardSize; r++){
+         isGood = true;
+         for (int i = 0; i < boardSize; i++){
+            if (board[z][r][i] != turn){
+               isGood = false;
+               break;
+            }
+         }
+         if (isGood)
+            return true;
+      }
+      // check rows
+      for (int c = 0; c < boardSize; c++){
+         isGood = true;
+         for (int i = 0; i < boardSize; i++){
+            if (board[z][i][c] != turn){
+               isGood = false;
+               break;
+            }
+         }
+         if (isGood)
+            return true;
+      }
+      // Diagnal left
       isGood = true;
-      for (int i = 0; i < boardSize; i++)
-      {
-         if (board[r][i] != turn)
-         {
+      for(int i=0; i < boardSize; i++){
+         if(board[z][i][i] != turn){
             isGood = false;
             break;
          }
       }
-      if (isGood)
+      if(isGood){
          return true;
-   }
-   for (int c = 0; c < boardSize; c++)
-   {
+      }
+      // Diagnal right
       isGood = true;
-      for (int i = 0; i < boardSize; i++)
-      {
-         if (board[i][c] != turn)
-         {
+      for(int i=0; i < boardSize; i++){
+         if(board[z][i][(boardSize - 1) - i] != turn){
             isGood = false;
             break;
          }
       }
-      if (isGood)
+      if(isGood){
          return true;
+      }
+   }  
+   
+   // check z-axis columns
+   for (int r = 0; r < boardSize; r++){
+      for (int c = 0; c < boardSize; c++){
+         isGood = true;
+         for (int i = 0; i < boardSize; i++){
+            if (board[i][r][c] != turn){
+               isGood = false;
+               break;
+            }
+         }
+         if (isGood)
+            return true;
+         isGood = true;
+         for (int i = 0; i < boardSize; i++)
+            if (board[i][i][c] != turn){
+               isGood = false;
+               break;
+            }
+         if (isGood)
+            return true;
+         isGood = true;
+         for (int i = 0; i < boardSize; i++)
+            if (board[i][r][i] != turn){
+               isGood = false;
+               break;
+            }
+         if (isGood)
+            return true;
+      }
    }
 
-      
+   //Diagnals along the center
+   //Top right
    isGood = true;
-   for(int i=0; i < boardSize; i++){
-      if(board[i][i] != turn){
+   for(int i = 0; i < boardSize; i++){
+      if(board[i][i][i] != turn){
          isGood = false;
          break;
       }
@@ -190,10 +253,11 @@ bool didWin(const char board[][boardSize], char turn)
    if(isGood){
       return true;
    }
-
+   
+   //Top left
    isGood = true;
-   for(int i=0; i < boardSize; i++){
-      if(board[i][(boardSize - 1) - i] != turn){
+   for(int i = 0; i < boardSize; i++){
+      if(board[i][i][(boardSize-1) - i] != turn){
          isGood = false;
          break;
       }
@@ -201,6 +265,31 @@ bool didWin(const char board[][boardSize], char turn)
    if(isGood){
       return true;
    }
+   
+   //Bottom right
+   isGood = true;
+   for(int i = 0; i < boardSize; i++){
+      if(board[i][(boardSize-1) - i][i] != turn){
+         isGood = false;
+         break;
+      }
+   }
+   if(isGood){
+      return true;
+   }
+   
+   //Bottom left
+   isGood = true;
+   for(int i = 0; i < boardSize; i++){
+      if(board[i][(boardSize-1) - i][(boardSize-1) - i] != turn){
+         isGood = false;
+         break;
+      }
+   }
+   if(isGood){
+      return true;
+   }
+   
    
    return false;
 }
